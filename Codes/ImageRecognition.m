b494ba5275534237ee819931aa8f1eb08cf05da2
jetfire948 <382787300@qq.com>
2018-07-12 20:30:31 +0800

    Add Basic Files

---
+++ Codes/ImageRecognition.m
@@ -0,0 +1,10 @@
+%Ö÷º¯Êý
+function diameter = ImageRecognition(imageName)
+    diameter = 0;
+    
+    image = imread(imageName);
+    
+    
+    FunctionAboutHistogram(image);
+    FunctionAboutDoubleGaussian(image);
+end

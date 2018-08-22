b56b6a799715ced369c661fadd528f6f60668c29
Xiaoyu Li <1211026012@qq.com>
2018-08-22 22:14:52 +0800

    delete some notes
    delete some notes and fix a bug

--- Codes/GUI.m
+++ Codes/GUI.m
@@ -1,37 +1,13 @@
 function varargout = GUI(varargin)
-% GUI MATLAB code for GUI.fig
-%      GUI, by itself, creates a new GUI or raises the existing
-%      singleton*.
-%
-%      H = GUI returns the handle to a new GUI or the handle to
-%      the existing singleton*.
-%
-%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in GUI.M with the given input arguments.
-%
-%      GUI('Property','Value',...) creates a new GUI or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before GUI_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to GUI_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help GUI
-
-% Last Modified by GUIDE v2.5 15-Jul-2018 16:12:57
-
-% Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
+
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
                    'gui_OpeningFcn', @GUI_OpeningFcn, ...
                    'gui_OutputFcn',  @GUI_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
+               
 if nargin && ischar(varargin{1})
     gui_State.gui_Callback = str2func(varargin{1});
 end
@@ -41,16 +17,8 @@ if nargout
 else
    gui_mainfcn(gui_State, varargin{:});
 end
-% End initialization code - DO NOT EDIT
 
-
-% --- Executes just before GUI is made visible.
 function GUI_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to GUI (see VARARGIN)
 
 %当前是否存在截图框
 global cutbool;
@@ -62,26 +30,13 @@ handles.output = hObject;
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes GUI wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
 
-
-% --- Outputs from this function are returned to the command line.
 function varargout = GUI_OutputFcn(hObject, eventdata, handles) 
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 
-% Get default command line output from handles structure
 varargout{1} = handles.output;
 
-
-% --- Executes on button press in choosebutton.
+%选择函数
 function choosebutton_Callback(hObject, eventdata, handles)
-% hObject    handle to choosebutton (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 
 %读文件
 [filepath,filename] = uigetfile({'*.jpg';'*.bmp'},'Select the Image');
@@ -94,20 +49,15 @@ else
     axes(handles.imageaxes);
     imshow(currentimage);
     handles.currentimage = currentimage;
-    
     % == 将文件路径和文件名保存到handles里面 == %
     handles.filepath = filepath;
     handles.filename = filename;
     
-    % Update handles structure
     guidata(hObject,handles);
 end
 
-% --- Executes on button press in cutbutton.
+%截图函数
 function cutbutton_Callback(hObject, eventdata, handles)
-% hObject    handle to cutbutton (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 
 global cutbool
 
@@ -126,27 +76,19 @@ if cutbool == false
     guidata(hObject,handles);
 end
 
-
-% --- Executes on button press in cancelbutton.
+%退出.
 function cancelbutton_Callback(hObject, eventdata, handles)
-% hObject    handle to cancelbutton (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+
 close all
 
-
-% --- Executes on button press in confirmbutton.
+%计算.
 function confirmbutton_Callback(hObject, eventdata, handles)
-% hObject    handle to confirmbutton (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 clc
 clear
 %D:\下载\978-7-302-46774-8MATLAB智能算法代码\Intelligent algorithm\10\s10_4\Lenna.bmp'));%
 close all
 img= imread('eg.jpg');%more quickly
 %D:\yaoxl\大创2\图集\KPIS\KPIS001.bmp
-
 %img=t(:,:,1);
 %imshow(img)
 %img=histeq(img);
